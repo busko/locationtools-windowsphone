014cd9930b66b4f946bba05289fa5c85c5a3842f
Warwick Bayne <baynewc1@student.op.ac.nz>
2012-11-28 16:29:36 +1300

    created a windows phone 8 project
    created a new windows 8 project for the tracker app

---
+++ Busko location tools/Busko location tools/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Busko_location_tools.Resources;
+
+namespace Busko_location_tools
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
